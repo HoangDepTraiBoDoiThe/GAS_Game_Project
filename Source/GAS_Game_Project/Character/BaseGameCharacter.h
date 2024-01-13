d708a324479ecfd521f5537b5205d55b9f6800d9
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-01-13 17:21:48 +0700

    init Base character classes

---
+++ Source/GAS_Game_Project/Character/BaseGameCharacter.h
@@ -0,0 +1,29 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseGameCharacter.generated.h"
+
+UCLASS()
+class GAS_GAME_PROJECT_API ABaseGameCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseGameCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
---
+++ Source/GAS_Game_Project/Character/Enemy/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS_Game_Project/Character/BaseGameCharacter.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_GAME_PROJECT_API AEnemyCharacter : public ABaseGameCharacter
+{
+	GENERATED_BODY()
+	
+};
