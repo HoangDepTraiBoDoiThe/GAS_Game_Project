2d85b748180a8310908e15cddf097be88e1170e0
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-01-13 17:09:47 +0700

    Setup Dependencies

--- Source/GAS_Game_Project/GAS_Game_Project.Build.cs
+++ Source/GAS_Game_Project/GAS_Game_Project.Build.cs
@@ -8,7 +8,7 @@ public class GAS_Game_Project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags", "UMG", "AIModule", "Niagara", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
