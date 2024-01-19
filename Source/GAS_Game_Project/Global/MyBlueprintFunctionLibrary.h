fc2ef678f354423323f55cc0cbf15e8d2253ad84
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-01-19 16:35:29 +0700

    init My blueprint function library

---
+++ Source/GAS_Game_Project/Global/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_GAME_PROJECT_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
