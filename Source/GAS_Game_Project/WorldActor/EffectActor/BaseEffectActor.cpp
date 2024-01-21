e276479e24b9fd7c8616cd4f2c3662e3c585bc6b
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-01-21 14:52:12 +0700

    Added some effect actors to the game

--- Source/GAS_Game_Project/WorldActor/EffectActor/BaseEffectActor.cpp
+++ Source/GAS_Game_Project/WorldActor/EffectActor/BaseEffectActor.cpp
@@ -5,49 +5,63 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "GameplayAbilityBlueprint.h"
 #include "Components/SphereComponent.h"
-#include "GAS_Game_Project/Character/BaseGameCharacter.h"
-#include "GAS_Game_Project/Character/Player/PlayerCharacter.h"
-#include "GAS_Game_Project/Character/Player/PlayerState/MyPlayerState.h"
-#include "GAS_Game_Project/GAS/AttributeSet/BaseAttributeSet.h"
+#include "GAS_Game_Project/GAS/MyAbilitySystemComponent.h"
 
 ABaseEffectActor::ABaseEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mesh"));
-	SetRootComponent(Mesh);
-	SphereComponent = CreateDefaultSubobject<USphereComponent>(FName("Sphere component"));
-	SphereComponent->SetupAttachment(GetRootComponent());
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(FName("Scene component"));
+	SetRootComponent(SceneComponent);
 }
 
 void ABaseEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlap);
-	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnEndOverlap);
 }
 
 void ABaseEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-	const FHitResult& SweepResult)
+                                 const FHitResult& SweepResult)
 {
-	UAbilitySystemComponent* AbilitySystemComponent =
-		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor);
-	if (AbilitySystemComponent)
-	{
-		FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
-		ContextHandle.AddSourceObject(this);
-		const FGameplayEffectSpecHandle Spec = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, EffectLevel, ContextHandle);
-		AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
-	}
+	if (!GetAbilitySystemComponent(OtherActor)) return;
+	if (EffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnBeginOverlap) DoApplyGameplayEffect(OtherActor);
 }
 
 void ABaseEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponent(OtherActor);
+	if (!ASC) return;
+	if (EffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap) DoApplyGameplayEffect(OtherActor);
+	if (ActiveGameplayEffectHandles.Num() > 0 && EffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		ASC->RemoveActiveGameplayEffect(*ActiveGameplayEffectHandles.Find(ASC), 1);
+		ActiveGameplayEffectHandles.Remove(ASC);
+	}
+}
+
+void ABaseEffectActor::DoApplyGameplayEffect(AActor* OtherActor)
+{
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponent(OtherActor);
+	if (!ASC) return;
+	FGameplayEffectContextHandle ContextHandle = ASC->MakeEffectContext();
+	ContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle Spec = ASC->MakeOutgoingSpec(GameplayEffectClass, EffectLevel, ContextHandle);
+	const FActiveGameplayEffectHandle ActiveGameplayEffectHandle = ASC->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
+
+	// What if the effect is infinite? And what if there are multiple game characters that overlap with this actor?
+	// We need to store the handle of the active gameplay effect to remove it later.
+	// Otherwise, only the last character that overlaps with this actor will have the effect removed.
+	// I guess :D
+	if (Spec.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite && EffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+		ActiveGameplayEffectHandles.Add(ASC, ActiveGameplayEffectHandle);
+}
+
+UAbilitySystemComponent* ABaseEffectActor::GetAbilitySystemComponent(AActor* TheActor)
+{
+	UAbilitySystemComponent* AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TheActor);
+	return AbilitySystemComponent;
 }
 
 
--- Source/GAS_Game_Project/WorldActor/EffectActor/BaseEffectActor.h
+++ Source/GAS_Game_Project/WorldActor/EffectActor/BaseEffectActor.h
@@ -3,12 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ActiveGameplayEffectHandle.h"
 #include "GameFramework/Actor.h"
 #include "BaseEffectActor.generated.h"
 
+class UMyAbilitySystemComponent;
+struct FActiveGameplayEffectHandle;
 class UGameplayEffect;
 class USphereComponent;
 
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy
+{
+	ApplyOnBeginOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy
+{
+	RemoveOnBeginOverlap,
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
 UCLASS()
 class GAS_GAME_PROJECT_API ABaseEffectActor : public AActor
 {
@@ -19,15 +36,22 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	UFUNCTION()
+	void DoApplyGameplayEffect(AActor* OtherActor);
+	UFUNCTION(BlueprintCallable)
 	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	static UAbilitySystemComponent* GetAbilitySystemComponent(AActor* TheActor);
 
+	UPROPERTY(EditAnywhere, Category=Policy)
+	EEffectApplicationPolicy EffectApplicationPolicy = EEffectApplicationPolicy::ApplyOnBeginOverlap;
+	UPROPERTY(EditAnywhere, Category=Policy)
+	EEffectRemovalPolicy EffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+
+	TMap<UAbilitySystemComponent*, FActiveGameplayEffectHandle> ActiveGameplayEffectHandles;
+	
 	UPROPERTY(VisibleAnywhere, Category = "Component")
-	TObjectPtr<UStaticMeshComponent> Mesh;
-	UPROPERTY(VisibleAnywhere, Category = "Component")
-	TObjectPtr<USphereComponent> SphereComponent;
+	TObjectPtr<USceneComponent> SceneComponent;
 
 	UPROPERTY(EditAnywhere, Category = "Gameplay ability system")
 	TSubclassOf<UGameplayEffect> GameplayEffectClass;
