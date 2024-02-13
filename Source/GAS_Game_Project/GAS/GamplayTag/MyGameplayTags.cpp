f67b6c4e7ce2feccf687d41d07d28f9a393c4b6d
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-02-13 10:45:13 +0700

    Added native gameplay tags

--- Source/GAS_Game_Project/GAS/GamplayTag/MyGameplayTags.cpp
+++ Source/GAS_Game_Project/GAS/GamplayTag/MyGameplayTags.cpp
@@ -3,5 +3,83 @@
 
 #include "MyGameplayTags.h"
 
+#include "GameplayTagsManager.h"
+
 MyGameplayTags MyGameplayTags::GameplayTags;
 
+void MyGameplayTags::InitNativeGameplayTags()
+{
+	/*
+	 *	Vital attributes
+	 */
+	MyGameplayTags::GameplayTags.Attribute_Vital_HitPoint = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Vital.HitPoint"),
+		FString(
+			"Numeric representation of a character's life force, indicating the amount of damage they can endure before succumbing to defeat."));
+
+	MyGameplayTags::GameplayTags.Attribute_Vital_Mana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Vital.Mana"),
+		FString(
+			"Magical energy or power, associated with casting spells or performing supernatural actions like casting spells"));
+	/*
+	*	Primary attributes
+	*/
+	MyGameplayTags::GameplayTags.Attribute_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Primary.Intelligence"), FString("Directly affect maximum mana and mana regeneration."));
+
+	MyGameplayTags::GameplayTags.Attribute_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Primary.Strength"), FString("Influence physical power."));
+
+	MyGameplayTags::GameplayTags.Attribute_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Primary.Resilience"),
+		FString("Impacts the character's resistances, including 'Armor' and 'Armor penetration.'"));
+
+	MyGameplayTags::GameplayTags.Attribute_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Primary.Vigor"),
+		FString("Impacts the character's 'Maximum HitPoint' and 'Heal Regeneration.'"));
+
+	/*
+	*	Secondary attributes
+	*/
+	MyGameplayTags::GameplayTags.Attribute_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.Armor"),
+		FString("Impacted by 'Resilience'. Reduces damage taken. Improve 'Block Chance' and 'Critical Hit Res'."));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.ArmorPenetration"),
+		FString("Impacted by 'Resilience'. Ignore percentage of enemy 'Armor'. Increases 'Critical Hit Chance'."));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.BlockChance"), FString("Impacted by 'Armor'. Chance to cut damage in half"));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.CriticalHitChance"),
+		FString("Impacted by 'ArmorPenetration'. Chance to double plus Critical Hit bonus."));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.CriticalHitDamage"),
+		FString("Impacted by 'ArmorPenetration'. Chance to plus damage."));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.CriticalHitResistance"),
+		FString("Impacted by 'Armor'. Reduces the critical hit chance of attacking enemies."));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.HealthRegeneration"),
+		FString("Impacted by 'Vigor'. Amount of 'HitPoint' regenerated every 1 second"));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.ManaRegeneration"),
+		FString("Impacted by 'Intelligence'. Amount of 'Mana' regenerated every 1 second"));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.MaxMana"),
+		FString("Impacted by 'Intelligence'. Maximum amount of 'Mana' obtainable"));
+
+	MyGameplayTags::GameplayTags.Attribute_Secondary_MaxHitPoint = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attribute.Secondary.MaxHitPoint"),
+		FString("Impacted by 'Vigor'. Maximum amount of 'Hit Point' obtainable"));
+
+
+
+}
--- Source/GAS_Game_Project/GAS/GamplayTag/MyGameplayTags.h
+++ Source/GAS_Game_Project/GAS/GamplayTag/MyGameplayTags.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 
 /**
  * 
@@ -11,7 +12,44 @@ class GAS_GAME_PROJECT_API MyGameplayTags
 {
 public:
  static const MyGameplayTags& Get() {return GameplayTags;}
+ static void InitNativeGameplayTags ();
 
+ /*
+ *	Vital attributes
+ */
+ FGameplayTag Attribute_Vital_HitPoint;
+ FGameplayTag Attribute_Vital_Mana;
+
+ /*
+  *	Primary attributes
+  */
+ FGameplayTag Attribute_Primary_Intelligence;
+ FGameplayTag Attribute_Primary_Strength;
+ FGameplayTag Attribute_Primary_Vigor;
+ FGameplayTag Attribute_Primary_Resilience;
+	
+ /*
+  *	Secondary attributes
+  */
+ FGameplayTag Attribute_Secondary_Armor;
+ FGameplayTag Attribute_Secondary_ArmorPenetration;
+ FGameplayTag Attribute_Secondary_BlockChance;
+ FGameplayTag Attribute_Secondary_CriticalHitChance;
+ FGameplayTag Attribute_Secondary_CriticalHitDamage;
+ FGameplayTag Attribute_Secondary_CriticalHitResistance;
+ FGameplayTag Attribute_Secondary_HealthRegeneration;
+ FGameplayTag Attribute_Secondary_ManaRegeneration;
+ FGameplayTag Attribute_Secondary_MaxHitPoint;
+ FGameplayTag Attribute_Secondary_MaxMana;
+
+ /*
+  * Input Tags 
+  */
+ FGameplayTag Control_Movement;
+ FGameplayTag Control_LMB;
+ FGameplayTag Control_RMB;
+ FGameplayTag Test_Ability_1;
+ 
 protected:
 
 private:
