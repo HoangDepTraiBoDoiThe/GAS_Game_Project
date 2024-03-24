83c35367c01e031657281430a32f059311a733d6
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-03-24 23:42:29 +0700

    PlayerController/feature: init blank custom enhanced input system

---
+++ Source/GAS_Game_Project/InputSystem/MyEnhancedInputComponent.h
@@ -0,0 +1,17 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "MyEnhancedInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_GAME_PROJECT_API UMyEnhancedInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+};
