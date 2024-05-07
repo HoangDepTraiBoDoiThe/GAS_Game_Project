b4566e68d541c0e6adf750d019a4d73b387fb12c
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-05-07 09:53:56 +0700

    Enemy AI/feature: Init custom service
    Find nearest target

---
+++ Source/GAS_Game_Project/BehaviorTree/Service/BTService_FindNearestTarget.cpp
@@ -0,0 +1,11 @@
+// Cu Nhat Hoang
+
+
+#include "BTService_FindNearestTarget.h"
+
+void UBTService_FindNearestTarget::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	
+}
---
+++ Source/GAS_Game_Project/BehaviorTree/Service/BTService_FindNearestTarget.h
@@ -0,0 +1,24 @@
+// Cu Nhat Hoang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlueprintBase.h"
+#include "BTService_FindNearestTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_GAME_PROJECT_API UBTService_FindNearestTarget : public UBTService_BlueprintBase
+{
+	GENERATED_BODY()
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere)
+	FBlackboardKeySelector TargetActor;
+	
+	UPROPERTY(EditAnywhere)
+	FBlackboardKeySelector DistanceToTarget;
+};
