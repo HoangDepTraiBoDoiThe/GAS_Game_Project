cc3f3c38e4b3280c0f68d6555f86edb94012674b
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-05-09 14:41:17 +0700

    Enemy AI/feature: Range Attack for range enemy

--- Source/GAS_Game_Project/WorldActor/Projectile/BaseProjectile.cpp
+++ Source/GAS_Game_Project/WorldActor/Projectile/BaseProjectile.cpp
@@ -46,6 +46,7 @@ void ABaseProjectile::Tick(float DeltaTime)
 void ABaseProjectile::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (Cast<APawn>(OtherActor) == GetInstigator()) return;
 	bIsOverlappedOnClient = true;
 	if (DestroyFX) UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), DestroyFX, GetActorLocation());
 	if (HasAuthority())
